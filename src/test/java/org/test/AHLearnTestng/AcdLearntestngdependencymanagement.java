6aeabf7dff4eb8cc83a445398f4e145878306829
Suhail <iamsuhail79@gmail.com>
2023-10-26 21:17:41 +0530

    26-oct first commit

---
+++ src/test/java/org/test/AHLearnTestng/AcdLearntestngdependencymanagement.java
@@ -0,0 +1,36 @@
+package org.test.AHLearnTestng;
+
+  /*
+
+Dependencies Management
+=======================
+
+If method1 gets failed then it won't execute rest of the methods
+ */
+
+
+import org.testng.annotations.Test;
+
+public class AczLearntestngdependencymanagement {
+
+
+    @Test
+    public void Method1() {
+        //System.out.println("this is method1");
+      
+
+    }
+
+
+    @Test(dependsOnMethods = "Method1")
+    public void Method2() {
+        System.out.println("this is method2");
+    }
+
+
+    @Test(dependsOnMethods = "Method2")
+    public void Method3() {
+        System.out.println("this is method3");
+    }
+
+}
---
+++ src/test/java/org/test/AHLearnTestng/Adblearntestnganalysertwo.java
@@ -0,0 +1,2 @@
+package org.test.AHLearnTestng;public class Adblearntestnganalysertwo {
+}
