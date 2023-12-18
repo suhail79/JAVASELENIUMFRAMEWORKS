01aee56dfd50319e38da02ccdcc98c8ba8e90431
Suhail <iamsuhail79@gmail.com>
2023-12-18 12:43:12 +0530

    18decfirstcommit

--- src/test/java/org/prac/seleniumpractice/pracprog1.java
+++ src/test/java/org/prac/seleniumpractice/pracprog1.java
@@ -5,31 +5,16 @@ import java.util.Map;
 public class pracprog1 {
     public static void main(String[] args) {
 
-        char Grade = 'E';
 
-        switch (Grade) {
+        String name = "Suhail";
+        int reverse = 0;
 
-            case 'S':
-                System.out.println(">90 <100 percentage");
-                break;
-            case 'A':
-                System.out.println(">80 <90 percentage");
-                break;
-            case 'B':
-                System.out.println(">70 80 percentage");
-                break;
-            case 'C':
-                System.out.println(">60 <50 percentage");
-                break;
-            case 'D':
-                System.out.println(">40 <50 percentage");
-                break;
-            default:
-                System.out.println("need to re appear for examination");
+        char[] charArray = name.toCharArray();
 
-
+        for (char c : charArray) {
+            reverse++;
         }
-
+        System.out.println(reverse);
 
     }
 }
